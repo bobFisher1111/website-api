dde0e964989a51d0f63d613191095e814ca09c0f
bobFisher1111 <rroseberry@twu.edu>
2024-02-28 17:16:41 -0600

    Vercel test 3

---
+++ api/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import router from '../src/routes';
+
+
+const app = express();
+import { PORT } from '../src/config';
+
+// may or may not need these
+app.use(cors({
+  credentials: true,
+}));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use(express.json());
+app.use('/api/v1/students', router);
+
+// const server = http.createServer(app);
+
+// locally
+// app.listen(8080, () => {
+//   console.log('Server running on http://localhost:8080/');
+// });
+
+// vercel
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:8080/');
+});
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,34 +1,34 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import router from '../routes';
+// import express from 'express';
+// import http from 'http';
+// import bodyParser from 'body-parser';
+// import cookieParser from 'cookie-parser';
+// import compression from 'compression';
+// import cors from 'cors';
+// import router from '../routes';
 
 
-const app = express();
-import { PORT } from '../config';
+// const app = express();
+// import { PORT } from '../config';
 
-// may or may not need these
-app.use(cors({
-  credentials: true,
-}));
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
+// // may or may not need these
+// app.use(cors({
+//   credentials: true,
+// }));
+// app.use(compression());
+// app.use(cookieParser());
+// app.use(bodyParser.json());
 
-app.use(express.json());
-app.use('/api/v1/students', router);
+// app.use(express.json());
+// app.use('/api/v1/students', router);
 
-// const server = http.createServer(app);
+// // const server = http.createServer(app);
 
-// locally
-// app.listen(8080, () => {
+// // locally
+// // app.listen(8080, () => {
+// //   console.log('Server running on http://localhost:8080/');
+// // });
+
+// // vercel
+// app.listen(3000, () => {
 //   console.log('Server running on http://localhost:8080/');
 // });
-
-// vercel
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:8080/');
-});
