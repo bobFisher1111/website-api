6fd29b7c4c4482531f5390220d21850979fcaaec
bobFisher1111 <rroseberry@twu.edu>
2024-02-28 18:12:01 -0600

    vercel test 5

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,10 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
+// import express from 'express';
+// import http from 'http';
+// import bodyParser from 'body-parser';
+// import cookieParser from 'cookie-parser';
+// import compression from 'compression';
+// import cors from 'cors';
+const express = require("express");
 import router from '../src/routes';
 
 
@@ -11,12 +12,12 @@ const app = express();
 import { PORT } from '../src/config';
 
 // may or may not need these
-app.use(cors({
-  credentials: true,
-}));
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
+// app.use(cors({
+//   credentials: true,
+// }));
+// app.use(compression());
+// app.use(cookieParser());
+// app.use(bodyParser.json());
 
 app.use(express.json());
 app.use('/api/v1/students', router);
